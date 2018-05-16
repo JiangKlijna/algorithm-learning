9e8db0c0acb6c1a5a9bcf4963ec81065f55c7db3
jiangKlijna <leil729564090@outlook.com>
2018-05-16 18:17:43 +0800

    :fire:add Stack

---
+++ src/util/Stack.java
@@ -0,0 +1,63 @@
+package util;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+public class Stack<E> {
+	private final List<E> data;
+
+	public Stack() {
+		this(LinkedList.class);
+	}
+
+	public Stack(Class<? extends List> cls) {
+		List<E> list;
+		try {
+			list = cls.newInstance();
+		} catch (ReflectiveOperationException e) {
+			list = new Vector();
+		}
+		data = list;
+	}
+
+	public E push(E item) {
+		data.add(item);
+		return item;
+	}
+
+	public E pop() {
+		return data.remove(size() - 1);
+	}
+
+	public E peek() {
+		return data.get(size() - 1);
+	}
+
+	public boolean empty() {
+		return size() == 0;
+	}
+
+	public int search(E o) {
+		return data.indexOf(o);
+	}
+
+	public int size() {
+		return data.size();
+	}
+
+	@Override
+	public String toString() {
+		return data.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return data.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return data.equals(obj);
+	}
+}
