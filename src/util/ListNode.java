4de4ea14031fd4bf38c4542bdc3da5989e59e289
jiangKlijna <leil729564090@outlook.com>
2018-05-18 19:14:33 +0800

    :fire:add ListNode

---
+++ src/util/ListNode.java
@@ -0,0 +1,25 @@
+package util;
+
+public class ListNode<E> {
+	private E val;
+	private ListNode next;
+
+	public ListNode(E x) {
+		val = x;
+	}
+
+	public static <E> ListNode getListNode(E... es) {
+		ListNode re = new ListNode(es[0]), prev = re;
+		for (int i = 1; i < es.length; i++)
+			prev = prev.next = new ListNode(es[i]);
+		return re;
+	}
+
+	public static void printListNode(ListNode ln) {
+		while (ln != null) {
+			System.out.print(ln.val + "\t");
+			ln = ln.next;
+		}
+		System.out.println();
+	}
+}
